a20613c588e66ad46a002ac308b42ac843cc0820
sunshineplan <sunshineplan@users.noreply.github.com>
2018-11-02 10:16:11 +0800

    Update lib/output.py
    Make csv file can directly open in Excel when containing Chinese characters

--- lib/output.py
+++ lib/output.py
@@ -3,8 +3,8 @@
 
 from csv import DictWriter
 
-def saveCSV(filename, fieldnames, content, encoding='utf-8'):
-    with open(filename, 'w', encoding=encoding, newline='') as output_file:
+def saveCSV(filename, fieldnames, content):
+    with open(filename, 'w', encoding='utf-8-sig', newline='') as output_file:
         output = DictWriter(output_file, fieldnames, extrasaction='ignore')
         output.writeheader()
         output.writerows(content)

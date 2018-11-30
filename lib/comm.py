4bcc53510128a40f0cf597c83ded0ac5bda60229
sunshineplan <sunshineplan@users.noreply.github.com>
2018-11-30 16:45:23 +0800

    split getAgents function to lib.comm

---
+++ lib/comm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+from bs4 import BeautifulSoup
+from urllib.request import Request
+from urllib.request import urlopen
+
+def getAgents():
+    try:
+        url = 'https://techblog.willshouse.com/2012/01/03/most-common-user-agents/'
+        request = Request(url, headers = {'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)'})
+        html = BeautifulSoup(urlopen(request), 'html.parser')
+        agent = html.find('textarea', class_='get-the-list')
+        agent = agent.text.splitlines()[:10]
+        error = 0
+    except:
+        agent = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.99',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36 Maxthon/5.2.5.3000',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6788.400 QQBrowser/10.3.2714.400',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER'
+            ]
+        error = 1
+    return agent, error
--- sitelib/amazon.py
+++ sitelib/amazon.py
@@ -18,6 +18,7 @@ from time import time
 from time import strftime
 from random import randint
 sys.path.append("..")
+from lib.comm import getAgents
 from lib.output import saveCSV
 
 import logging
@@ -37,7 +38,11 @@ class Amazon():
     def __init__(self, keyword, path=''):
         self.keyword = keyword
         self.quoteKeyword = quote(keyword)
-        self.agent = self.getAgents()
+        self.agent, error = getAgents()
+        if error == 0:
+            logger.debug('Download user agents list successful.')
+        else:
+            logger.debug('Download user agents list failed. Use custom list instead.')
         self.accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
         self.acceptEncoding = 'gzip, deflate, br'
         self.acceptLanguage = 'en-US,en;q=0.9'
@@ -45,31 +50,6 @@ class Amazon():
         self.fieldnames = ['Name', 'Author', 'Price', 'URL']
         self.storepath = path
         self.filename = 'AZ' + strftime('%Y%m%d') + '-' + self.keyword + '.csv'
-
-    def getAgents(self):
-        try:
-            url = 'https://techblog.willshouse.com/2012/01/03/most-common-user-agents/'
-            request = Request(url, headers = {'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)'})
-            html = BeautifulSoup(urlopen(request), 'html.parser')
-            agent = html.find('textarea', class_='get-the-list')
-            agent = agent.text.splitlines()[:10]
-            logger.debug('Download user agents list successful.')
-        except:
-            agent = [
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.99',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36 Maxthon/5.2.5.3000',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6788.400 QQBrowser/10.3.2714.400',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER'
-                ]
-            logger.debug('Download user agents list failed. Use custom list instead.')
-        return agent
 
     def getHeaders(self):
         for attempts in range(10):
